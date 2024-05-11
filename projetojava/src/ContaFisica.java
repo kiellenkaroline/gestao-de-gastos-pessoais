bd66c75786879719fdbe8f1b88213f7212380994
karol <kiellenkarolinne123@icloud.com>
2024-05-10 23:53:00 -0300

    Classe Conta fisica recriada

---
+++ projetojava/src/ContaFisica.java
@@ -0,0 +1,47 @@
+public class ContaFisica  extends Pessoa{
+
+    private int id;
+    private double saldo;
+    private String cpf;
+
+    public String getCpf(){
+        return cpf;
+    }
+
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public void depositarFisica(Transacao transacao) {
+        double saldoAtual = getSaldo();
+        double valorDepositado = Transacao.getValor();
+        double novoSaldo = saldoAtual + valorDepositado;
+        setSaldo(novoSaldo);
+    }
+
+    public void sacarFisica(Transacao transacao) {
+        double saldoAtual = getSaldo();
+        double valorDepositado = Transacao.getValor();
+        double novoSaldo = saldoAtual - valorDepositado;
+        setSaldo(novoSaldo);
+    }
+
+
+    public void add(ContaFisica contas) {
+    }
+}
