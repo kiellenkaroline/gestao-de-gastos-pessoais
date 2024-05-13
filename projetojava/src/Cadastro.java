74813fd669b9f672310c9ffe1c017e257c6d0a3a
Andre Moreira <andreluiz.moreira@hotmail.com>
2024-05-13 15:50:41 -0300

    alteracao no cadastro e na main

--- projetojava/src/Cadastro.java
+++ projetojava/src/Cadastro.java
@@ -1,9 +1,35 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Cadastro {
     private List<Pessoa> pessoas = new ArrayList<>();
     private List<ContaFisica> contas = new ArrayList<>();
+    Scanner sc = new Scanner(System.in);
+    public ContaFisica cadastrarContaFisica() {
+        ContaFisica cf = new ContaFisica();
+
+        System.out.println("Seja Bem Vindo ao Cadastro de Conta: ");
+        System.out.println("Modalidade: Pessoa Fisica");
+
+        System.out.print("Digite seu nome: ");
+        cf.setNome(sc.nextLine());
+
+        System.out.print("Digite sua idade: ");
+        cf.setIdade(sc.nextInt());
+
+        System.out.print("Digite seu CPF: ");
+        cf.setCpf(sc.next());
+
+        System.out.println("Conta criada com sucesso!");
+        System.out.println("Carregando Informacoes...");
+        System.out.println("Dados da conta: ");
+        System.out.println("Nome: " + cf.getNome());
+        System.out.println("Idade: " + cf.getIdade());
+        System.out.println("CPF: " + cf.getCpf());
+
+        return cf; //
+    }
 
     public void cadastrarPessoa( Pessoa pessoa){
         pessoas.add(pessoa);
--- projetojava/src/Main.java
+++ projetojava/src/Main.java
@@ -1,33 +1,10 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         ContaFisica cf = new ContaFisica();
         Transacao t = new Transacao();
         Cadastro cd = new Cadastro();
-        Pessoa pessoa = new Pessoa();
-        ContaFisica conta = new ContaFisica();
 
-        System.out.println("Seja Bem Vindo ao Cadastro de Conta: ");
-        System.out.println("Modalidade: Pessoa Fisica");
-
-        System.out.print("Digite seu nome: ");
-        cf.setNome(sc.nextLine());
-
-        System.out.print("Digite sua idade: ");
-        cf.setIdade(sc.nextInt());
-
-        System.out.print("Digite seu CPF: ");
-        cf.setCpf(sc.next());
-
-        System.out.println("Conta criada com sucesso!");
-        System.out.println("Carregando Informacoes...");
-        System.out.println("Dados da conta: ");
-        System.out.println("Nome: " + cf.getNome());
-        System.out.println("Idade: " + cf.getIdade());
-        System.out.println("CPF: " + cf.getCpf());
-
+        cd.cadastrarContaFisica();
         cf.depositarFisica(t);
         cf.sacarFisica(t);
     }
